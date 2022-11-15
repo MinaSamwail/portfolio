ed08592830561d5f430217a3db53cb214fb78868
Mina <minasamwail@gmail.com>
2022-11-15 16:58:22 +0100

    ajout des scripts ventura, nettoyage de l'app.html et test de ventura

--- src/app/front-page/components/home/home.component.ts
+++ src/app/front-page/components/home/home.component.ts
@@ -9,22 +9,13 @@ declare var VANTA: any;
 export class HomeComponent implements AfterViewInit  {
 
   ngAfterViewInit(): void {
-    // VANTA.BIRDS({
-    //   el: '#vanta', // element selector string or DOM object reference
-    //   color: 0x000000,
-    //   waveHeight: 20,
-    //   shininess: 50,
-    //   waveSpeed: 1.5,
-    //   zoom: 0.75
-    // })
-
-    VANTA.CLOUDS({
-      el: "#vanta",
-      mouseControls: true,
-      touchControls: true,
-      gyroControls: false,
-      minHeight: 200.00,
-      minWidth: 200.00
+    VANTA.BIRDS({
+      el: '#vanta', // element selector string or DOM object reference
+      color: 0x000000,
+      waveHeight: 20,
+      shininess: 50,
+      waveSpeed: 1.5,
+      zoom: 0.75
     })
   }
 }
